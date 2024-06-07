891f800ead315408ec1bf1b5eaf6dce3853804e3
bipashabg <bipasha_bg@es.iitr.ac.in>
2024-06-07 21:01:09 +0530

    adds home page ui

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <main className="main-content">
+        <div className="header-line">Heart of Robotics at R-Land</div>
+        <h2>
+          <span className="gradient-text">Models</span> and 
+          <span className="gradient-text"> Robotics</span> Section, IIT Roorkee
+        </h2>
+        <p>We're a student group, bonded by our passion for Robotics. Delving into programming, electronics, and AI through projects and workshops.</p>
+        <button>Our Team</button>
+      </main>
+    </div>
+  );
+};
+
+export default Homepage;
