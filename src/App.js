339781ffef190b2ea83270afc227594df43514b2
shashankkosta157 <shashankkosta940@gmail.com>
2024-07-31 20:06:46 +0530

    Teamspage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './components/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./components/Home";
 import { useState } from "react";
-
+import Cards from "./Card/Cards";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-
-  const [display, setDisplay] = useState(['about', 'help']);
+  const [display, setDisplay] = useState(["about", "help"]);
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Homepage display={display} setDisplay={setDisplay} />} />
+          <Route
+            path="/"
+            element={<Homepage display={display} setDisplay={setDisplay} />}
+          />
+          <Route path="/Teams" element={<Cards />} />
+
+          {/* <Route path="/Home" element={<Home />} /> */}
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,47 @@
-import React, { Component } from 'react'
-import '../styles/Navbar.css'
-import Teams from '../components/Teams' 
+import React, { Component } from "react";
+import "../styles/Navbar.css";
 
 class Navbar extends Component {
   // const[showMediaIcons,setShowMediaIcons]= useState(false);
   state = { clicked: false };
   handleClick = () => {
-    this.setState({ clicked: !this.state.clicked })
-  }
- 
-  render(){
-    
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
     return (
-     
-      <div className='navbar'>
+      <div className="navbar">
         <div className="container">
-          <div className='Left'>
-          <img src="/logo.png" alt="MaRS Logo" className="logo" />
+          <div className="Left">
+            <img src="/logo.png" alt="MaRS Logo" className="logo" />
             <h1>MaRS</h1>
           </div>
-
-          <div className='Right'>
-            <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
-              <li className="item"><a href="#" className="c">Home</a></li>
-              <li className="item"><a href="#" className="c">Projects</a></li>
-              <li className="item"><a href="#" className="c">Gallery</a></li>
-              <li className="item c" > <Teams/></li>
-              <li><div className='search-box'>Blogs</div></li>
+          <div className="Right">
+            <ul
+              id="navbar"
+              className={this.state.clicked ? "#navbar active" : "#navbar"}
+            >
+              <li className="item">
+                <a href="/" className="c">
+                  Home
+                </a>
+              </li>
+              <li className="item">
+                <a href="#" className="c">
+                  Projects
+                </a>
+              </li>
+              <li className="item">
+                <a href="#" className="c">
+                  Gallery
+                </a>
+              </li>
+              <li className="item ">
+                <a href="/Teams" className="c">Teams</a>
+              </li>
+              <li>
+                <div className="search-box">Blogs</div>
+              </li>
             </ul>
             {/* <div className="hamburgmenu">
          <a href='#'>
@@ -35,14 +50,17 @@ class Navbar extends Component {
       </div> */}
             {/* onClick="() => setShowMediaIcons(!showMediaIcons)" */}
           </div>
-          <div id="mobile" onClick={this.handleClick} >
-            <a className="hamburg"><i id='bar' className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i></a>
+          <div id="mobile" onClick={this.handleClick}>
+            <a className="hamburg">
+              <i
+                id="bar"
+                className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+              ></i>
+            </a>
           </div>
         </div>
       </div>
-
-    )
-    
+    );
   }
 }
-export default Navbar
+export default Navbar;
