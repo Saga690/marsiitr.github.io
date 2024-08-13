10f5168004091916606c52b6332fe3e1b4321ac3
Saga690 <master.ayush.2003@gmail.com>
2024-08-13 22:26:06 +0530

    converted navbar to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,77 @@
-import React, { Component, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/Navbar.css";
 
-class Navbar extends Component {
+const Navbar = () => {
 
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+  const [clicked, setClicked] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
   };
 
-  
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-  render() {
-    return (
-      <div className="navbar">
-        <div className="container">
-          <div className="Left">
-            <img src="/logo.png" alt="MaRS Logo" className="logo" />
-            <h1>MaRS</h1>
-          </div>
-          <div className="Right">
-            <ul
-              id="navbar"
-              className={this.state.clicked ? "#navbar active" : "#navbar"}
-            >
-              <li className="item">
-                <a href="/" className="c">
-                  Home
-                </a>
-              </li>
-              <li className="item">
-                <a href="#" className="c">
-                  Projects
-                </a>
-              </li>
-              <li className="item">
-                <a href="#" className="c">
-                  Gallery
-                </a>
-              </li>
-              <li className="item ">
-                <a href="/Teams" className="c">Teams</a>
-              </li>
-              <li>
-                <div className="search-box">Blogs</div>
-              </li>
-            </ul>
-            {/* <div className="hamburgmenu">
-         <a href='#'>
-          <GiHamburgerMenu />
-         </a>
-      </div> */}
-            {/* onClick="() => setShowMediaIcons(!showMediaIcons)" */}
-          </div>
-          <div id="mobile" onClick={this.handleClick}>
-            <a className="hamburg">
-              <i
-                id="bar"
-                className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-              ></i>
-            </a>
-          </div>
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+
+  return (
+    <div className={`navbar ${scrolled ? "scrollednav" : ""}`}>
+      <div className="container">
+        <div className="Left">
+          <img src="/logo.png" alt="MaRS Logo" className="logo" />
+          <h1>MaRS</h1>
+        </div>
+        <div className="Right">
+          <ul
+            id="navbar"
+            className={clicked ? "#navbar active" : "#navbar"}
+          >
+            <li className="item">
+              <a href="/" className="c">
+                Home
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="c">
+                Projects
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="c">
+                Gallery
+              </a>
+            </li>
+            <li className="item ">
+              <a href="/Teams" className="c">Teams</a>
+            </li>
+            <li>
+              <div className="search-box">Blogs</div>
+            </li>
+          </ul>
+        </div>
+        <div id="mobile" onClick={handleClick}>
+          <a className="hamburg">
+            <i
+              id="bar"
+              className={clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Navbar;
