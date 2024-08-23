3f4d08a7f699f32f18704f5775cbbb0793959590
Saga690 <master.ayush.2003@gmail.com>
2024-08-23 20:07:13 +0530

    added achievements page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Gallery from "./components/gallery/Gallery";
+import Achievements from "./components/Achievements";
 
 function App() {
   const [display, setDisplay] = useState(["about", "help"]);
@@ -24,6 +25,7 @@ function App() {
             />
             <Route path="/Teams" element={<Cards />} />
             <Route path="/Gallery" element={<Gallery />} />
+            <Route path="/achievements" element={<Achievements />} />
 
             {/* <Route path="/Home" element={<Home />} /> */}
           </Routes>
---
+++ src/components/api/achievements.js
@@ -0,0 +1,56 @@
+const achievements = [
+    {
+        title: "Winner",
+        event_name: "WRO India",
+        icon: "https://cdn-icons-png.flaticon.com/128/3662/3662817.png",
+        iconBg: "#383E56",
+        date: "March 2024",
+        points: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        ],
+    },
+    {
+        title: "First Runner Up",
+        event_name: "Technician",
+        icon: "https://cdn-icons-png.flaticon.com/128/1693/1693746.png",
+        iconBg: "#E6DEDD",
+        date: "Sept 2023 - Oct 2023",
+        points: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        ],
+    },
+    {
+        title: "Winner",
+        event_name: "Robotfest",
+        icon: "https://cdn-icons-png.flaticon.com/128/6584/6584942.png",
+        iconBg: "#383E56",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        ],
+    },
+    {
+        title: "Second Runner Up",
+        event_name: "VEX Worlds",
+        icon: "https://cdn-icons-png.flaticon.com/128/10817/10817309.png",
+        iconBg: "#E6DEDD",
+        date: "Apr 2021 - Dec 2021",
+        points: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        ],
+    },
+];
+
+export {achievements};
